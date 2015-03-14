134a403053b509426b24d00f2d1f2b155f96a2a1
Pakpoom Subsoontorn <pakpoomton@gmail.com>
2015-03-14 18:41:13 +0000

    add text for main doxygen page

---
+++ doxmain.py
@@ -0,0 +1,6 @@
+## \mainpage Welcome to Cellmodeller 4 documentation
+#  Here, you can find detail about the organization of files, classes and methods in Cellmodeller 4
+#  You can try many things here lalalala I wanna eat you YEAH!
+#
+#
+#
