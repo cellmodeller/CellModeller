85fb4bcf2c9a32589882f8f7f72b2dd2a5659c70
Anton Kan <antonkan@dhcp-172-17-194-134.eduroam.wireless.private.cam.ac.uk>
2015-05-27 20:18:18 +0100

    changing tutorial

--- Examples/ex1_simpleGrowth.py
+++ Examples/ex1_simpleGrowth.py
@@ -5,11 +5,7 @@ from CellModeller.GUI import Renderers
 import numpy
 import math
 
-cell_cols = {   0:[0.7,0.2,0], \
-                1:[0,0.2,0.7], \
-                2:[0.2,0.7,0], \
-                3:[0.7,0.2,0.7], \
-                4:[0.2,0.7,0.7]}
+cell_cols = {0:[0,1.0,0], 1:[1.0,0,0]}
 
 def setup(sim):
     # Set biophysics, signalling, and regulation models
@@ -20,14 +16,8 @@ def setup(sim):
     # Only biophys and regulation
     sim.init(biophys, regul, None, None)
  
-
     # Specify the initial cell and its location in the simulation
-    for i in range(5):
-        px = random.uniform(-10.0,10.0)
-        py = random.uniform(-10.0,10.0)
-        dx = random.uniform(0,1.0)
-        dy = math.sqrt(1-dx*dx)
-        sim.addCell(cellType=i, pos=(px,py,0), dir=(dx,dy,0)) 
+    sim.addCell(cellType=0, pos=(0,0,0), dir=(0,0,0))
 
     # Add some objects to draw the models
     therenderer = Renderers.GLBacteriumRenderer(sim)
@@ -39,11 +29,11 @@ def init(cell):
     cell.targetVol = 3.5 + random.uniform(0.0,0.5)
     # Specify growth rate of cells
     cell.growthRate = 1.0
+    cell.color = cell_cols[cell.cellType]
 
 def update(cells):
     #Iterate through each cell and flag cells that reach target size for division
     for (id, cell) in cells.iteritems():
-        cell.color = cell_cols[cell.cellType]
         if cell.volume > cell.targetVol:
             cell.divideFlag = True
 
