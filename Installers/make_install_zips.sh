cd4d633f62bd2a6d3e57d52d890ed0b0ca8f5aa0
Tim Rudge <timrudge@gmail.com>
2017-10-10 17:52:00 -0300

    Do not make installers for private repo, defunct

--- Installers/make_install_zips.sh
+++ Installers/make_install_zips.sh
@@ -9,7 +9,6 @@ VERSION=`git describe`
 INSTALLERDIR=InstallCellModeller-$VERSION-OSX
 mkdir -p $INSTALLERDIR
 mkdir -p $INSTALLERDIR/PackageFiles
-cp $THIS_DIR/Miniconda-3.8.3-MacOSX-x86_64.sh $INSTALLERDIR/PackageFiles/
 cp -Rf $THIS_DIR/../Examples $INSTALLERDIR/PackageFiles/
 cp -Rf $THIS_DIR/../Scripts $INSTALLERDIR/PackageFiles/
 cp -Rf $THIS_DIR/../Examples $INSTALLERDIR/PackageFiles/
@@ -22,28 +21,10 @@ chmod +x $INSTALLERDIR/InstallCellModeller
 tar -czf $INSTALLERDIR.tgz $INSTALLERDIR 
 rm -rf $INSTALLERDIR
 
-# OSX from private repo
-INSTALLERDIR=InstallCellModeller-$VERSION-OSX-dev
-mkdir -p $INSTALLERDIR
-mkdir -p $INSTALLERDIR/PackageFiles
-cp $THIS_DIR/Miniconda-3.8.3-MacOSX-x86_64.sh $INSTALLERDIR/PackageFiles/
-cp -Rf $THIS_DIR/../Examples $INSTALLERDIR/PackageFiles/
-cp -Rf $THIS_DIR/../Scripts $INSTALLERDIR/PackageFiles/
-cp -Rf $THIS_DIR/../Examples $INSTALLERDIR/PackageFiles/
-cp -Rf $THIS_DIR/../Doc $INSTALLERDIR/PackageFiles/
-cp $THIS_DIR/cmpython $INSTALLERDIR/PackageFiles/
-cp $THIS_DIR/cmgui $INSTALLERDIR/PackageFiles/
-cp -f $THIS_DIR/InstallCellModellerOSX_private.command $INSTALLERDIR/InstallCellModeller
-cp -f $THIS_DIR/UninstallCellModeller $INSTALLERDIR/UninstallCellModeller
-chmod +x $INSTALLERDIR/InstallCellModeller
-tar -czf $INSTALLERDIR.tgz $INSTALLERDIR 
-rm -rf $INSTALLERDIR
-
 # Linux 64bit from public repo
 INSTALLERDIR=InstallCellModeller-$VERSION-Linux64
 mkdir -p $INSTALLERDIR
 mkdir -p $INSTALLERDIR/PackageFiles
-cp $THIS_DIR/Miniconda-3.8.3-Linux-x86_64.sh $INSTALLERDIR/PackageFiles/
 cp -Rf $THIS_DIR/../Examples $INSTALLERDIR/PackageFiles/
 cp -Rf $THIS_DIR/../Scripts $INSTALLERDIR/PackageFiles/
 cp -Rf $THIS_DIR/../Examples $INSTALLERDIR/PackageFiles/
@@ -51,23 +32,6 @@ cp -Rf $THIS_DIR/../Doc $INSTALLERDIR/PackageFiles/
 cp $THIS_DIR/cmpython $INSTALLERDIR/PackageFiles/
 cp $THIS_DIR/cmgui $INSTALLERDIR/PackageFiles/
 cp -f $THIS_DIR/InstallCellModellerLinux64_public.sh $INSTALLERDIR/InstallCellModeller.sh
-cp -f $THIS_DIR/UninstallCellModeller $INSTALLERDIR/UninstallCellModeller
-chmod +x $INSTALLERDIR/InstallCellModeller.sh
-tar -czf $INSTALLERDIR.tgz $INSTALLERDIR 
-rm -rf $INSTALLERDIR
-
-# Linux 64bit from private repo
-INSTALLERDIR=InstallCellModeller-$VERSION-Linux64-dev
-mkdir -p $INSTALLERDIR
-mkdir -p $INSTALLERDIR/PackageFiles
-cp $THIS_DIR/Miniconda-3.8.3-Linux-x86_64.sh $INSTALLERDIR/PackageFiles/
-cp -Rf $THIS_DIR/../Examples $INSTALLERDIR/PackageFiles/
-cp -Rf $THIS_DIR/../Scripts $INSTALLERDIR/PackageFiles/
-cp -Rf $THIS_DIR/../Examples $INSTALLERDIR/PackageFiles/
-cp -Rf $THIS_DIR/../Doc $INSTALLERDIR/PackageFiles/
-cp $THIS_DIR/cmpython $INSTALLERDIR/PackageFiles/
-cp $THIS_DIR/cmgui $INSTALLERDIR/PackageFiles/
-cp -f $THIS_DIR/InstallCellModellerLinux64_private.sh $INSTALLERDIR/InstallCellModeller.sh
 cp -f $THIS_DIR/UninstallCellModeller $INSTALLERDIR/UninstallCellModeller
 chmod +x $INSTALLERDIR/InstallCellModeller.sh
 tar -czf $INSTALLERDIR.tgz $INSTALLERDIR 
