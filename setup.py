08a7c89b4aad72dde34cc86afc29e1039872b0bb
Tim Rudge <timrudge@gmail.com>
2020-04-01 20:06:49 -0300

    Add all requirements to setup.py

--- setup.py
+++ setup.py
@@ -22,10 +22,8 @@ except:
 
 
 setup(name='CellModeller',
-    install_requires=['numpy', 'scipy', 'pyopengl', 'pyopencl', 'reportlab', 'matplotlib'],
-    setup_requires=['numpy', 'scipy', 'pyopengl', 'pyopencl', 'reportlab', 'matplotlib'],
-    #setup_requires=['python=2'],
-    #setup_requires=['numpy', 'pyopengl', 'mako', 'pyopencl'],
+    install_requires=['numpy', 'scipy', 'pyopengl', 'mako', 'pyqt5', 'pyopencl', 'reportlab', 'matplotlib'],
+    setup_requires=['numpy', 'scipy', 'pyopengl', 'mako', 'pyqt5', 'pyopencl', 'reportlab', 'matplotlib'],
     packages=['CellModeller',
                 'CellModeller.Biophysics',
                 'CellModeller.Biophysics.BacterialModels',
@@ -35,5 +33,6 @@ setup(name='CellModeller',
                 'CellModeller.Signalling',
                 'CellModeller.GUI'],
      package_data={'':['*.cl','*.ui']},
+     python_requires='>=3',
      version=str(version_git)
 )
